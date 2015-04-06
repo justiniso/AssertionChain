7043b8120d390ba671595ccb7fb9d430acac5ff8
Justin <justin@shapeways.com>
2015-04-05 21:20:32 -0400

    some configuration stuff for pypi

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ from setuptools import setup
 
 setup(
     name='assertionchain',
+    description='Utility for chaining commands and incrementally checking the results',
+    packages=['assertionchain'],
     version='0.1.0',
     author='Justin Iso',
+    url='https://github.com/justiniso/assertionchain',
+    download_url='https://github.com/justiniso/assertionchain/tarball/0.1.0',
     install_requires=[],
     tests_require=[],
     test_suite='tests',
-    setup_requires=[],
-    description='',
-    url='',
+    setup_requires=[]
 )
