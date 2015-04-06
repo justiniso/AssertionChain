9a48d7361c2f4cc7a623c68eac9270a97bb3de14
Justin <justin@shapeways.com>
2015-04-05 21:26:15 -0400

    add author email

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     description='Utility for chaining commands and incrementally checking the results',
     version='0.1.0',
     author='Justin Iso',
+    author_email='justin+assertionchain@justiniso.com',
     url='https://github.com/justiniso/assertionchain',
     download_url='https://github.com/justiniso/assertionchain/tarball/0.1.0',
     install_requires=[],
